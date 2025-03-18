606b6e32d99847307351fa4ff5bbbb07933e654e
ishanrana363 <ishanrana363@gmail.com>
2025-03-18 20:26:46 +0600

    slider two page design completed

--- src/pages/auction/AuctionPage.jsx
+++ src/pages/auction/AuctionPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AuctionBanner from "./AuctionBanner";
 import AuctionSlider from "./AuctionSlider";
+import AutionSliderTwo from "./AutionSliderTwo";
 
 const AuctionPage = () => {
   return (
     <div>
         <AuctionBanner></AuctionBanner>
         <AuctionSlider></AuctionSlider>
+        <AutionSliderTwo></AutionSliderTwo>
     </div>
   );
 };
