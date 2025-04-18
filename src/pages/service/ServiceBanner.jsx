ecd636dbabf6ce3c7962e544d5fba540129742e3
ishanrana363 <ishanrana363@gmail.com>
2025-04-18 12:16:06 +0600

    term & condiction modal open

--- src/pages/service/ServiceBanner.jsx
+++ src/pages/service/ServiceBanner.jsx
@@ -5,13 +5,13 @@ const ServiceBanner = () => {
   return (
     <div className="  mb-2  " >
       <div className="  " >
-      <p className="lg:pt-20 pt-24 text-[#403730]  font-semibold text-center  ">
+      <p className="lg:pt-20 pt-20 text-[#403730]  font-semibold text-center  ">
         Our services
       </p>
       <h1 className="lg:text-5xl text-2xl p-2 lg:leading-16 text-center lg:mt-3 text-[#263234] font-semibold">
         Our Services
       </h1>
-      <div className="lg:mt-6 max-w-[800px] pb-24 mx-auto text-center">
+      <div className="lg:mt-6 max-w-[800px] lg:pb-24 mx-auto text-center">
         <p className="text-[#263234] text-justify p-2 text-[16px] lg:text-xl lg:leading-[30px]">
           A Virtue Hope C.I.C., we provide holistic, trauma-informed services
           designed to support women on their journey to healing, empowerment,
