cace84a9b2aff553c48d06f5ea5c2e38019f8033
ishanrana363 <ishanrana363@gmail.com>
2025-06-19 10:24:16 -0800

    podcast page design modify

--- src/helper/imgUrl.js
+++ src/helper/imgUrl.js
@@ -1,1 +1,5 @@
-export const imgUrl = `http://137.59.180.219:8000`;
+// local img url 
+// export const imgUrl = `http://137.59.180.219:8000`; 
+
+// live image url 
+export const imgUrl = `http://217.154.61.198/api`; 
--- src/pages/hooks/useAxiosPublic.jsx
+++ src/pages/hooks/useAxiosPublic.jsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 // const backendUrl = `http://localhost:3000/api/v1`;
-const backendUrl = `http://137.59.180.219:8000/api`;
+// local url 
+// const backendUrl = `http://137.59.180.219:8000/api`; 
+
+// live url live url 
+const backendUrl = `http://217.154.61.198/api`; 
 
 const axiosPublic = axios.create({
   baseURL: backendUrl,
