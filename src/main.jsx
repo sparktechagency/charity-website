1ff3abeb14871d69a5b31d73457224a685a4d901
almas <almashossain7384@gmail.com>
2025-06-22 14:58:48 +0600

    css file changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./pages/dashboard/dashboard.css"
+import "../src/assets/css/font.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.jsx";
 import "@ant-design/v5-patch-for-react-19";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import { Toaster } from "react-hot-toast";
-import "../src/assets/css/font.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
